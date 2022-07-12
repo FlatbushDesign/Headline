d45f1806de2d567882b219811d96d48fed622e21
Luca Faggianelli <luca.faggianelli@toptal.com>
2022-07-12 17:26:02 +0200

    zoom user info

--- headline/zoom.py
+++ headline/zoom.py
@@ -1,7 +1,6 @@
 from operator import itemgetter
 import httpx
 
-from headline.models import User
 from headline.provider import Provider, Credentials
 
 
@@ -10,14 +9,34 @@ class ZoomCredentials(Credentials):
     authorize_url = "https://zoom.us/oauth/authorize"
     token_url = "https://zoom.us/oauth/token"
     scopes = [
+        # "user:write:admin",
+        # "user:read:admin",
+        # "user:read",
+        # "user:write",
+        # "user_profile",
+        "user_info:read",
         "meeting:read",
     ]
+
+    # async def get_user_info(self, credentials: dict):
+    #     async with httpx.AsyncClient(
+    #         base_url="https://api.zoom.us/v2",
+    #         headers={"Authorization": f"Bearer {credentials['access_token']}"},
+    #     ) as client:
+    #         response = await client.get("/users/me")
+
+    #         if response.is_error:
+    #             print(response.json())
+
+    #         response.raise_for_status()
+
+    #         return response.json()
 
 
 class Zoom(Provider):
     name = "zoom"
 
-    async def run(self, data: dict, user_credentials: dict, user: User):
+    async def run(self, data: dict, user_credentials: dict):
         async with httpx.AsyncClient(
             base_url="https://api.zoom.us/v2",
             headers={"Authorization": f"Bearer {user_credentials.get('access_token')}"},
